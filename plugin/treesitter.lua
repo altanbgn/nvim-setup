fa259e6f8500a1e5706cbb8c432217477c3905e1
Altanbagana <altanbagana@protonmail.com>
2023-11-08 10:53:08 +0800

    lmao

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,16 +1,11 @@
 require("nvim-treesitter.configs").setup {
   sync_install = false,
   auto_install = true,
-
-  indent = {
-    enable = true
-  },
-
+  indent = { enable = true },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = { "markdown" },
   },
-
   ensure_installed = {
     "bash",
     "comment",
